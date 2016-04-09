f31f6e16c5c6624d88cf7bfd47d5bcfbc4b588b2
Brian Frølund <brianfroelund@gmail.com>
2016-04-09 17:04:37 +0200

    updated monitor controller script to use hdmi control

--- monitor_controller.py
+++ monitor_controller.py
@@ -1,28 +1,52 @@
 import RPi.GPIO as GPIO
 import time
+import os
+import logging
 from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO, 
+                    filename='monitor_controller.log', # log to this file
+                    format='%(asctime)s %(message)s') # include timestamp
+
+logging.info("Starting Monitor Controller")
+
+def turnMonitorOff():
+	logging.info("Turning monitor off")
+	os.system("/opt/vc/bin/tvservice -p && /opt/vc/bin/tvservice -o")
+	time.sleep(5)
+
+def turnMonitorOn():
+	logging.info("Turning monitor on")
+	os.system("/opt/vc/bin/tvservice -p && chvt 1 && chvt 7")
+    time.sleep(5)
+
+sensor = 18
+timeout = 60
 
 GPIO.setmode(GPIO.BCM)
-
-monitor = 15
-sensor = 18
-
-timeout = 300
-
 GPIO.setup(sensor, GPIO.IN)
-GPIO.setup(monitor, GPIO.OUT)
 
-GPIO.output(monitor, 0) #ON
 lastsensed = datetime.now()
+turnMonitorOn()
+isMonitorOn = True
 
 while True:
-	if GPIO.input(sensor) == 1: 
-		lastsensed = datetime.now()
-		GPIO.output(monitor, 0) #ON
-		time.sleep(10)
-	elif (datetime.now() - lastsensed).seconds > timeout:
-		GPIO.output(monitor, 1) #OFF
-		time.sleep(1)
-	
-
+    if GPIO.input(sensor) == 1:
+            lastsensed = datetime.now()
+            if isMonitorOn == False:
+                    turnMonitorOn()
+                    isMonitorOn = True
+            time.sleep(1)
+    elif (datetime.now() - lastsensed).seconds > timeout:
+            if isMonitorOn == True:
+                    turnMonitorOff()
+                    isMonitorOn = False
+            time.sleep(1)
+logging.info("Stopping Monitor Controller")
 GPIO.cleanup()
+
+
+
+
+
